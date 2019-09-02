7d38183e63eb7f0271a70a963540b5c5a871f945
Paul Taylor <ptaylor@ricoh.com.au>
2019-09-02 10:51:58 +1000

    refine google place search

--- src/App.svelte
+++ src/App.svelte
@@ -4,14 +4,18 @@
   let map;
   let service;
   let infowindow;
+  let infowindowContent;
   let location;
+  let input;
+  let autocomplete;
+  let marker2;
 
   onMount(() => {
     initMap();
   });
 
   function initMap() {
-    location = new google.maps.LatLng(-33.866387,151.208332); //qvb
+    location = new google.maps.LatLng(-33.866387, 151.208332); //qvb
 
     infowindow = new google.maps.InfoWindow();
 
@@ -20,7 +24,7 @@
       zoom: 15
     });
 
-    var request = {
+    let request = {
       query: "Museum of Contemporary Art Australia",
       fields: ["name", "geometry"]
     };
@@ -35,6 +39,67 @@
         map.setCenter(results[0].geometry.location);
         console.log(results);
       }
+    });
+
+    input = document.getElementById("pac-input");
+    autocomplete = new google.maps.places.Autocomplete(input);
+    infowindow = new google.maps.InfoWindow();
+    infowindowContent = document.getElementById("infowindow-content");
+    infowindow.setContent(infowindowContent);
+
+    marker2 = new google.maps.Marker({
+      map: map,
+      anchorPoint: new google.maps.Point(0, -29)
+    });
+
+    autocomplete.addListener("place_changed", function() {
+      infowindow.close();
+      marker2.setVisible(false);
+      let place = autocomplete.getPlace();
+      if (!place.geometry) {
+        // User entered the name of a Place that was not suggested and
+        // pressed the Enter key, or the Place Details request failed.
+        window.alert("No details available for input: '" + place.name + "'");
+        return;
+      }
+
+      // If the place has a geometry, then present it on a map.
+      if (place.geometry.viewport) {
+        map.fitBounds(place.geometry.viewport);
+        console.log("mapped fitted to viewport");
+      } else {
+        map.setCenter(place.geometry.location);
+        map.setZoom(17); // Why 17? Because it looks good.
+        console.log("map centre set to "+ place.geometry.location);
+      }
+      marker2.setPosition(place.geometry.location);
+      marker2.setVisible(true);
+      console.log(place.geometry);
+      console.log("lat/lng="+place.geometry.location.lat()+"/"+place.geometry.location.lng());
+
+      var address = "";
+      if (place.address_components) {
+        address = [
+          (place.address_components[0] &&
+            place.address_components[0].short_name) ||
+            "",
+          (place.address_components[1] &&
+            place.address_components[1].short_name) ||
+            "",
+          (place.address_components[2] &&
+            place.address_components[2].short_name) ||
+            ""
+        ].join(" ");
+      } else {
+        console.log("no place.address_components");
+      }
+
+      console.log(place);
+
+      infowindowContent.children["place-icon"].src = place.icon;
+      infowindowContent.children["place-name"].textContent = place.name;
+      infowindowContent.children["place-address"].textContent = address;
+      infowindow.open(map, marker2);
     });
   }
 
@@ -68,6 +133,7 @@
     width: 40%;
     min-width: 340px;
     border: 3px solid black;
+    color: darkslategrey;
   }
 
   #map {
@@ -75,10 +141,28 @@
     width: 50%;
     min-width: 340px;
   }
+
+  #pac-input {
+    border: 1px dashed grey;
+    min-width: 21rem;
+  }
+
+  #infowindow-content {
+    /* display: none; */
+  }
 </style>
 
 <h1>Google places</h1>
 <div class="container">
-  <div class="input" />
+  <div class="input">
+    <label for="pac-input">Location:</label>
+    <input id="pac-input" type="text" placeholder="Enter a location" />
+  </div>
   <div id="map" />
+  <div id="infowindow-content">
+    <img src="" width="16" height="16" id="place-icon" alt="" />
+    <span id="place-name" class="title" />
+    <br />
+    <span id="place-address" />
+  </div>
 </div>
